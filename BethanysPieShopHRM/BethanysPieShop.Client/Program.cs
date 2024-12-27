611e45e299e9dead2d8fc682bb87aed671f1d5a6
Luis Gustavo de Andrade Kokumai <luis.kokumai@aveva.com>
2024-12-27 14:32:06 -0300

    added employee information to client cache to avoid uneeded api calls

--- BethanysPieShopHRM/BethanysPieShop.Client/Program.cs
+++ BethanysPieShopHRM/BethanysPieShop.Client/Program.cs
@@ -1,11 +1,13 @@
 using BethanysPieShop.Client;
 using BethanysPieShop.Client.Services;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IEmployeeDataService, ClientEmployeeDataService>();
 
 await builder.Build().RunAsync();
--- BethanysPieShopHRM/BethanysPieShop.Client/Services/ClientEmployeeDataService.cs
+++ BethanysPieShopHRM/BethanysPieShop.Client/Services/ClientEmployeeDataService.cs
@@ -1,16 +1,20 @@
 ﻿using BethanysPieShopHRM.Shared.Domain;
+using Blazored.LocalStorage;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using BethanysPieShop.Client.Helper;
 
 namespace BethanysPieShop.Client.Services
 {
     public class ClientEmployeeDataService : IEmployeeDataService
     {
         private readonly HttpClient? _httpClient;
+        private readonly ILocalStorageService _localStorageService;
 
-        public ClientEmployeeDataService(HttpClient httpClient)
+        public ClientEmployeeDataService(HttpClient httpClient, ILocalStorageService localStorageService)
         {
             _httpClient = httpClient;
+            _localStorageService = localStorageService;
         }
 
         public Task<Employee> AddEmployee(Employee employee)
@@ -25,7 +29,25 @@ namespace BethanysPieShop.Client.Services
 
         public async Task<IEnumerable<Employee>> GetAllEmployees()
         {
-            var list = await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>( await _httpClient.GetStreamAsync("/api/employee"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            bool employeeExpirationExists = await _localStorageService.ContainKeyAsync(LocalStorageConstants.EmployeesListExpirationKey);
+            if (employeeExpirationExists)
+            {
+                DateTime employeeListExpiration = await _localStorageService.GetItemAsync<DateTime>(LocalStorageConstants.EmployeesListExpirationKey);
+                if (employeeListExpiration > DateTime.Now)//get from local storage
+                {
+                    if (await _localStorageService.ContainKeyAsync(LocalStorageConstants.EmployeesListKey))
+                    {
+                        return await _localStorageService.GetItemAsync<List<Employee>>(LocalStorageConstants.EmployeesListKey);
+                    }
+                }
+            }
+
+            var list = await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
+                    (await _httpClient.GetStreamAsync("api/employee"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            await _localStorageService.SetItemAsync(LocalStorageConstants.EmployeesListKey, list);
+            await _localStorageService.SetItemAsync(LocalStorageConstants.EmployeesListExpirationKey, DateTime.Now.AddMinutes(1));
+
             return list;
         }
 
